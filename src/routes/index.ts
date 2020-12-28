00dbbc5497818512176fa68354217d345520ff47
Leandro Freitas <leandrofreitas@Leandros-MBP.lan.netimoveis.com.br>
2020-12-28 11:24:50 -0300

    criando o middleware de auth

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,33 @@
-import { Request, Response } from 'express';
-
+import * as jwt from 'jsonwebtoken';
 import { UserController } from '../controller/User';
 import { AuthController } from '../controller/Auth';
+
+// Middleware
+function auth(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    res.status(401).json({
+      auth: false,
+      message: 'No token'
+    });
+  }
+
+  jwt.verify(
+    token.replace('Bearer ', ''),
+    process.env.SECRET,
+    function (err, decoded) {
+      if (err) {
+        return res.status(500).json({
+          auth: false,
+          message: 'This is not good, an error occurred'
+        });
+      }
+
+      req.user = decoded.user;
+      next();
+    });
+}
 
 export class Routes {
 
@@ -10,7 +36,7 @@ export class Routes {
 
   public routes(app): void {
     app.route('/user')
-      .get(this.userController.list)
+      .get(auth, this.userController.list)
       .post(this.userController.create);
     
     app.route('/user/:id')
