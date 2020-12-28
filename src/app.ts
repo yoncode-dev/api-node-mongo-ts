276801f405e301da5ee4350bf59e98953a2767ad
Leandro Freitas <leandrofreitas@Leandros-MBP.lan.netimoveis.com.br>
2020-12-28 09:55:28 -0300

    configurando o controller nas rotas e mongo

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,30 @@ import * as bodyParser from 'body-parser';
 
 import { Routes } from './routes';
 
+import * as mongoose from 'mongoose';
+
 class App {
   public app: express.Application; 
   public routeApi: Routes = new Routes();
+
+  public mongoUrl: string = 'mongodb://localhost:55000/apits';
 
   constructor() {
     this.app = express();
     this.config();
     this.routeApi.routes(this.app);
+    this.mongoSetup();
   }
 
   private config(): void {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+  private mongoSetup(): void {
+    (<any>mongoose).Promise = global.Promise;
+    mongoose.connect(this.mongoUrl,
+      { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
   }
 }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,31 +1,19 @@
 import { Request, Response } from 'express';
 
+import { UserController } from '../controller/User';
+
 export class Routes {
+
+  public userController: UserController = new UserController();
+
   public routes(app): void {
     app.route('/user')
-      .get((req: Request, res: Response) => {
-        res.status(200).send({
-          message: 'get request ok',
-        });
-      }).post((req: Request, res: Response) => {
-        res.status(200).send({
-          message: 'post',
-        });
-      });
+      .get(this.userController.list)
+      .post(this.userController.create);
     
     app.route('/user/:id')
-      .get((req: Request, res: Response) => {
-        res.status(200).send({
-          message: 'get one',
-        });
-      }).put((req: Request, res: Response) => {
-        res.status(200).send({
-          message: 'put',
-        });
-      }).delete((req: Request, res: Response) => {
-        res.status(200).send({
-          message: 'delete',
-        });
-      });
+      .get(this.userController.show)
+      .put(this.userController.edit)
+      .delete(this.userController.remove);
   }
 }
