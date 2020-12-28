5e3746a435b1fff6b7a56f9f5e8f77a6a51cbdb9
Leandro Freitas <leandrofreitas@Leandros-MBP.lan.netimoveis.com.br>
2020-12-28 09:19:28 -0300

    criado a rota de crud do usuário

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,30 @@ import { Request, Response } from 'express';
 
 export class Routes {
   public routes(app): void {
-    app.route('/').get((req: Request, res: Response) => {
-      res.status(200).send({
-        message: 'get request ok',
-      })
-    });
+    app.route('/user')
+      .get((req: Request, res: Response) => {
+        res.status(200).send({
+          message: 'get request ok',
+        });
+      }).post((req: Request, res: Response) => {
+        res.status(200).send({
+          message: 'post',
+        });
+      });
+    
+    app.route('/user/:id')
+      .get((req: Request, res: Response) => {
+        res.status(200).send({
+          message: 'get one',
+        });
+      }).put((req: Request, res: Response) => {
+        res.status(200).send({
+          message: 'put',
+        });
+      }).delete((req: Request, res: Response) => {
+        res.status(200).send({
+          message: 'delete',
+        });
+      });
   }
 }
