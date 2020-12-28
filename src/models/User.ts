47ddd6beaddd2ae2f0ad05a2165e74454976fa16
Leandro Freitas <leandrofreitas@Leandros-MBP.lan.netimoveis.com.br>
2020-12-28 10:36:22 -0300

    implementando a senha encriptada

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,16 +1,40 @@
 import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+let mongooseHidden = require('mongoose-hidden')();
 
 const Schema = mongoose.Schema;
+const saltRounds = 10;
 
-export const UserSchema = new Schema({
+export interface UserProps extends mongoose.Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const UserSchema = new Schema<UserProps>({
   name: String,
   email: {
     type: String,
     unique: true,
   },
-  password: String,
+  password: {
+    type: String,
+    hide: true,
+  },
   create_date: {
     type: Date,
     default: Date.now
   }
+});
+
+UserSchema.plugin(mongooseHidden, { hidden: { _id: false } });
+
+UserSchema.pre<UserProps>('save',async function (next) {
+  
+  const newPassword = await bcrypt.hash(this.password, saltRounds);
+
+  this.password = newPassword;
+
+  next();
 });
