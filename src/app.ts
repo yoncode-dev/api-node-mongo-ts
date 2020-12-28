00947276f271f35116f272d8ead6fbb2ce1b9f65
Leandro Freitas <leandrofreitas@Leandros-MBP.lan.netimoveis.com.br>
2020-12-28 09:09:52 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+import { Routes } from './routes';
+
+class App {
+  public app: express.Application; 
+  public routeApi: Routes = new Routes();
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routeApi.routes(this.app);
+  }
+
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
+}
+
+export default new App().app;
